1ee9d723b80e4dded7df4c52bc8b9f6351dffe42
Green Geek <hari.molabanti@gmail.com>
2023-09-20 20:40:50 -0700

    get basic layout building

--- keyboards/aki27/cocot46plus/rules.mk
+++ keyboards/aki27/cocot46plus/rules.mk
@@ -10,7 +10,7 @@ BOOTLOADER = caterina
 ENCODER_ENABLE = yes
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+EXTRAKEY_ENABLE = no       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
@@ -26,8 +26,8 @@ SRC += matrix.c
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = adns5050
 
-OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
+OLED_ENABLE     = no
+# OLED_DRIVER     = SSD1306
 
 # Disable unnecessary functions to reduce firmware size
 SPACE_CADET_ENABLE = no
